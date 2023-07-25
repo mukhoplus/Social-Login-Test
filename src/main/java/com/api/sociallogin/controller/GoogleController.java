59fbb408e9fe3489895650dcb219bfb119ee6bb9
Mukho <mukho@kakao.com>
2023-07-26 00:28:05 +0900

    구글 로그인 API 구현

--- src/main/java/com/api/sociallogin/controller/GoogleController.java
+++ src/main/java/com/api/sociallogin/controller/GoogleController.java
@@ -1,4 +1,39 @@
 package com.api.sociallogin.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.api.sociallogin.service.impl.GoogleService;
+import com.api.sociallogin.util.ApiUtil;
+
+@Controller
+@RequestMapping("/login/google")
 public class GoogleController {
+
+	GoogleService googleService = GoogleService.getInstance();
+	ApiUtil apiUtil = new ApiUtil();
+
+	@GetMapping
+	public String googleLogin() {
+		return "redirect:" + googleService.getLoginUrl();
+	}
+
+	@GetMapping("/callback")
+	public String getGoogleInfo(HttpServletRequest request, Model model) {
+		String accessTokenUrl = googleService.getAccessTokenUrl(request);
+		String accessToken = apiUtil.getAccessToken(accessTokenUrl);
+		HashMap<String, String> userInfo = googleService.getUserInfo(accessToken);
+
+		model.addAttribute("email", userInfo.get("email"));
+		model.addAttribute("token", userInfo.get("token"));
+		System.out.println("구글: " + userInfo.get("email") + " " + userInfo.get("token"));
+		return "google";
+	}
+
 }
--- src/main/java/com/api/sociallogin/service/impl/GoogleService.java
+++ src/main/java/com/api/sociallogin/service/impl/GoogleService.java
@@ -1,4 +1,63 @@
 package com.api.sociallogin.service.impl;
 
-public class GoogleService {
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.api.sociallogin.service.ApiService;
+import com.api.sociallogin.util.ApiKey;
+import com.api.sociallogin.util.ApiUtil;
+import com.google.gson.JsonObject;
+
+public class GoogleService extends ApiService {
+
+	private static GoogleService instance;
+	public ApiUtil apiUtil = new ApiUtil();
+	public ApiKey apiKey = new ApiKey();
+
+	public static GoogleService getInstance() {
+		if (instance == null)
+			instance = new GoogleService();
+		return instance;
+	}
+
+	public GoogleService() {
+		clientId = apiKey.getGoogleClientId();
+		redirectURI = apiKey.getGoogleRedirectURI();
+		state = apiUtil.generateRandomState();
+	}
+
+	@Override
+	public String getLoginUrl() {
+		return "https://accounts.google.com/o/oauth2/v2/auth?client_id=" + clientId + "&redirect_uri=" + redirectURI
+			+ "&response_type=code&scope=email";
+	}
+
+	@Override
+	public String getAccessTokenUrl(HttpServletRequest request) {
+		String code = request.getParameter("code");
+		return "https://oauth2.googleapis.com/token?code=" + code + "&client_id=" + clientId + "&client_secret="
+			+ apiKey.getGoogleClientSecret() + "&redirect_uri=" + redirectURI + "&grant_type=authorization_code";
+	}
+
+	@Override
+	public HashMap<String, String> getUserInfo(String accessToken) {
+		HashMap<String, String> userInfo = new HashMap<>();
+
+		String header = "Bearer " + accessToken;
+		String profileUrl = "https://www.googleapis.com/userinfo/v2/me";
+
+		Map<String, String> requestHeaders = new HashMap<>();
+		requestHeaders.put("Authorization", header);
+
+		String responseBody = apiUtil.get(profileUrl, requestHeaders);
+		// response 전체를 parsing
+		JsonObject profileObject = apiUtil.gson.fromJson(responseBody, JsonObject.class);
+
+		userInfo.put("token", accessToken);
+		userInfo.put("email", profileObject.get("email").getAsString());
+
+		return userInfo;
+	}
 }
